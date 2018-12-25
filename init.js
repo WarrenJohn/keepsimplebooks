b2c111c56db0ea5f13d3e82315cb8e76b03d131f
WarrenJohn <warrenjohn89@gmail.com>
2018-12-24 20:06:01 -0500

    Added a tutorial link to the others for later reference

--- init.js
+++ init.js
@@ -3,7 +3,9 @@ require('dotenv').config({path:'../.env'});
 // // csv API: https://csv.js.org/parse/api/
 // // watch this: https://www.youtube.com/watch?v=j55fHUJqtyw&list=PLillGF-RfqbYSx-Ab1xWVanGKtowTsnNm
 // // from: https://stackoverflow.com/questions/41776978/how-to-read-csv-file-in-node-js
+
 // // Vue: https://www.youtube.com/watch?v=5LYrN_cAJoA&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa
+// // another: https://www.youtube.com/watch?v=Fa4cRMaTDUI
 // const fs = require('fs');
 // var parse = require('csv-parse');
 // fs.readFile("accountactivity.csv", function (err, fileData) {
