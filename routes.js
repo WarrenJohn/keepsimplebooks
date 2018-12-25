18a347bb4c18109304c9999dfb0c7b47afad4bba
WarrenJohn <warrenjohn89@gmail.com>
2018-12-24 22:37:28 -0500

    Removed vue branch testing

--- routes.js
+++ routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 require('dotenv').config({path:'../.env'});
 const app = express();
 const port = process.env.PORT;
-
-app.use(express.static('static'));
+const views = path.join(__dirname, 'views');
+app.use(express.static('./static'));
 app.use(bodyParser.json());
+
+
 /* How do I render plain HTML?
 You don’t! There’s no need to “render” HTML with the res.render() function. If you have a specific file,
 use the res.sendFile() function.
@@ -14,7 +17,9 @@ If you are serving many assets from a directory, use the express.static() middle
 
 app.get('/', (req, res) =>{
     console.log('GET: index');
-    res.send('Index Page Test');
+    res.sendFile(path.join(views, '/index.html'));
+    // Vue will work if I write the code directly in a script tag
+    // in the index.html file, but not if I link the index.js in the script tag.
     }
 );
 app.get('/robots.txt', (req, res) =>{
