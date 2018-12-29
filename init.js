4b1df58e03d51ea871ee1409b2993b6928295c0b
WarrenJohn <warrenjohn89@gmail.com>
2018-12-29 15:46:16 -0500

    Added vue docs as comment for easy reference

--- init.js
+++ init.js
@@ -11,7 +11,7 @@ app.use(express.static('./static'));
 app.use(bodyParser.json());
 // app.set('views', path.join(__dirname + 'views'));
 require('./routes')(app);
-
+// Vue docs: https://cli.vuejs.org/guide/
 m.keepsimple_db.authenticate()
     .then(() => {
         // Test connection
