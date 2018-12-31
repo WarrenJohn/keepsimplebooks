8de7dd54cb6718e551f339c2071cf834dd0f8bea
WarrenJohn <warrenjohn89@gmail.com>
2018-12-31 15:35:53 -0500

    Set up routes for new pages added

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,12 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/dashboard">Dashboard</router-link> |
+      <router-link to="/history">History</router-link> |
+      <router-link to="/upload">Upload</router-link> |
+      <router-link to="/transactions">Transactions</router-link>
     </div>
     <router-view/>
   </div>
--- client/src/router.js
+++ client/src/router.js
@@ -8,18 +8,19 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
+    {path: '/', name: 'home', component: Home},
+    {path: '/about', name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+  },
+  {path: '/register', name: 'register', component: () => import ('./views/Register.vue')},
+  {path: '/login', name: 'login', component: () => import ('./views/Login.vue')},
+  {path: '/dashboard', name: 'dashboard', component: () => import ('./views/Dashboard.vue')},
+  {path: '/history', name: 'history', component: () => import ('./views/History.vue')},
+  {path: '/transactions', name: 'transactions', component: () => import ('./views/Transactions.vue')},
+  {path: '/upload', name: 'upload', component: () => import ('./views/Upload.vue')}
+
   ]
 });
