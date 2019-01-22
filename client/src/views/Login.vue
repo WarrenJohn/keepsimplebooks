cc05855b092802f71db59c8d8f251e4b19a11459
WarrenJohn <warrenjohn89@gmail.com>
2019-01-22 16:29:50 -0500

    added basic template for login and register pages

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="login">
+  <div class="text-center">
     <h1>This is the Login page</h1>
+    <input type="email" name="email" placeholder="Email"
+        v-model="email"/>
+    <br />
+    <input type="password" name="password" placeholder="Password"
+        v-model="password"/>
+    <br />
+    <button>Login</button>
   </div>
 </template>
+<script>
+export default{
+    data(){
+        return {
+            email: '',
+            password: ''
+        }
+    }
+}
+</script>
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>
+  <div class="text-center">
     <h1>This is the Register page</h1>
-    <input type="email" name="email" placeholder="Email" />
+    <input type="email" name="email" placeholder="Email"
+        v-model="email"/>
     <br />
-    <input type="password" name="password" placeholder="Password" />
+    <input type="password" name="password" placeholder="Password"
+        v-model="password"/>
     <br />
+    <input type="password" name="confirmpassword" placeholder="Confirm password"
+        v-model="confirmPassword"/>
     <button>Register</button>
   </div>
 </template>
+<script>
+export default{
+    data(){
+        return {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+}
+</script>
