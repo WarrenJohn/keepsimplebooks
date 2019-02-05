c4cd5612b5d0439edec1c2454466e10c29738ce6
WarrenJohn <warrenjohn89@gmail.com>
2019-02-05 13:30:34 -0500

    updated todo list

--- init.js
+++ init.js
@@ -23,7 +23,7 @@ const todo = `
 - Unit tests
 - Limit user requests to prevent spamming (express-rate-limit or something similar)
 - Convert complicated template variables to computed properties
-- SORTING Options: Add date range selection functionality to Dashboard.vue, Transactions.vue & History.vue
+- SORTING Options: Add date range selection functionality to Dashboard.vue, Transactions.vue & History.vue, sort by alphabetical, etc..
 - Dashboard charts, money flow in and out
 \x1b[0m`;
 
