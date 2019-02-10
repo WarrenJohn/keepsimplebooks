eeba4cacbdd24da24ab6fc0617bd6788c245679a
WarrenJohn <warrenjohn89@gmail.com>
2019-02-10 17:21:48 -0500

    removed unused function

--- utils.js
+++ utils.js
@@ -31,23 +31,3 @@ module.exports.registerUser = user => {
 
     return errors;
 }
-
-// possible future use
-module.exports.handleCSV_row = file => {
-    // inserts csv data row by row
-    fs.readFile(file, function (err, fileData) {
-        parse(fileData, {columns: false, trim: true}, function(err, data) {
-            // CSV data is in an array of arrys passed to this callback as rows.
-            data.forEach(row => {
-                m.insertRowBank({
-                    transaction_date: row[0],
-                    description: row[1],
-                    withdrawl: row[2],
-                    deposit: row[3],
-                    balance: row[4],
-                    user: 'warren'
-                });
-            });
-        });
-    });
-};
