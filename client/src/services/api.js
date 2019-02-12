0d058866da198c5c61dc66c135b4e394b89daf32
WarrenJohn <warrenjohn89@gmail.com>
2019-02-12 18:59:42 -0500

    changed vue api route to include 'api/'

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import store from '../store';
 
 export default () => {
     return axios.create({
-        baseURL: 'http://localhost:5000/',
+        baseURL: 'API/',
         headers: {'Authorization': `Bearer ${store.state.token}`}
     });
 };
