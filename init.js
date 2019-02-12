83ee727606afb3b18137d23e53a34ec6e90bc18b
WarrenJohn <warrenjohn89@gmail.com>
2019-02-11 20:19:05 -0500

    updated init

--- init.js
+++ init.js
@@ -39,9 +39,9 @@ app.use(cors());
 
 if (process.env.NODE_ENV === 'production'){
     // static folder
-    app.use(express.static(__dirname + '/public/'))
+    app.use(express.static(__dirname + '/client/dist/'))
     // handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/client/index.html'))
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/client/dist/index.html'))
 }
 
 require('./routes')(app);
